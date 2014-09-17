f9e57ef8fb6de4eafb8e394ae49d0922a3b42f7a
88soham <88soham@gmail.com>
2014-09-17 16:57:53 -0400

    matrix inversion commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions take an invertible matrix and finds the inverse and caches it 
+## to avoid recomputation in future
 
-## Write a short comment describing this function
+## makeCacheMatrix() takes an invertible matrix, returns a list of functions: set, get, setinverse and getinverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) 
+    {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() takes the list, checks if the inverse has already been computed. If so, returns it, else computes it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
